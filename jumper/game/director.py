140aa9a8c16b740aa9847aef1c95d98d3c2b1c10
lance-drake <dra15006@byui.edu>
2021-03-17 15:48:22 -0600

    return index for guess in word

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,7 +1,18 @@
 class Director:
+    self.guess=[]
+    self.word=''
     
     def __init__(self):
-        pass
+        self.guess=[]
+        self.word=''
 
     def start_game(self):
         pass
+
+
+    def find(self, guess):
+        self.guess.append(guess)
+        return [i for i, ltr in enumerate(self.word) if ltr == guess]
+
+
+
